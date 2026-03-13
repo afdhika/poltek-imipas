c50daad401013f3b8eff88860e20ffd8e26e8146
afdhika <afdhikasyahputra@gmail.com>
2026-03-14 01:56:36 +0700

    Vercel: Speed-insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next"
 import { Inter, Poppins } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css"
 
 const inter = Inter({
@@ -45,6 +46,7 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
